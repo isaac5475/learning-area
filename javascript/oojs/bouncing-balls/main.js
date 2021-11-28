d3cb5dd1cf0a1672aa306f5e7a6fb4d8c46b852e
isaac5475 <murar390@gmail.com>
2021-11-28 20:48:00 +0100

    added basic functionality

--- javascript/oojs/bouncing-balls/main.js
+++ javascript/oojs/bouncing-balls/main.js
@@ -10,5 +10,64 @@ const height = canvas.height = window.innerHeight;
 
 function random(min, max) {
   const num = Math.floor(Math.random() * (max - min + 1)) + min;
-  return num;
+  return num
 }
+  function Ball(x, y, velX, velY, color, size) {
+    this.x = x;
+    this.y = y;
+    this.velX = velX;
+    this.velY = velY;
+    this.color = color;
+    this.size = size;
+  }
+  Ball.prototype.draw = function() {
+    ctx.beginPath();
+    ctx.fillStyle = this.color;
+    ctx.arc(this.x, this.y, this.size, 0, 2*Math.PI);
+    ctx.fill();
+  }
+  Ball.prototype.update = function() {
+    if ((this.x + this.size) >= width) {
+      this.velX = -(this.velX);
+    }
+    if ((this.x - this.size) <= 0) {
+      this.velX = -(this.velX);
+    }
+
+    if ((this.y - this.size) <= 0) {
+      this.velY = -(this.velY);
+    }
+    if ((this.y + this.size) >= height) {
+      this.velY = -(this.velY);
+    }
+    this.x += this.velX;
+    this.y += this.velY;
+
+  }
+
+  let Balls = [];
+  while (Balls.length < 25) {
+    let size = random(10, 20);
+    let ball = new Ball(
+      random(0 + size, width - size),
+      random(size, height - size),
+      random(-7, 7),
+      random(-7, 7),
+      'rgb(' + random(0,255) + ',' + random(0,255) + ',' + random(0,255) +')',
+      size
+    );
+    Balls.push(ball);
+  }
+  function loop() {
+    ctx.fillStyle = 'rgba(0,0,0,0.25)';
+    ctx.fillRect(0,0, width, height);
+
+    for (let i = 0; i < Balls.length; i++) {
+      Balls[i].draw();
+      Balls[i].update();
+    }
+  requestAnimationFrame(loop);
+
+  }
+  loop();
+
